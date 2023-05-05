9be1f3b2de386e1f37e22aafcd404c4ddc9052d0
Anas Firly <anasfirly20@gmail.com>
2023-05-06 00:46:57 +0300

    fixed date picker

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -25,83 +25,54 @@ import {
 import { profileEdit } from "../Styles/ProfileEdit";
 
 // Components
-import DatePicker from "react-native-date-picker";
-import { DatePickerInput } from "react-native-paper-dates";
-import DateTimePicker from "@react-native-community/datetimepicker";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
 
 const Input = ({ label, value, onChangeText, keyboardType, type }) => {
-  const [date, setDate] = useState(new Date());
-  const [showPicker, setShowPicker] = useState(false);
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [datePickerVisible, setDatePickerVisible] = useState(false);
 
-  const toggleDatePicker = () => {
-    setShowPicker(!showPicker);
+  const showDatePicker = () => {
+    setDatePickerVisible(true);
   };
 
-  const onChange = ({ type }, selectedDate) => {
-    if (type === "set") {
-      const currentDate = selectedDate;
-      setDate(currentDate);
-
-      if (Platform.OS === "android") {
-        toggleDatePicker();
-        setDate(currentDate.toDateString());
-      }
-    } else {
-      toggleDatePicker();
-    }
+  const hideDatePicker = () => {
+    setDatePickerVisible(false);
   };
 
-  const confirmIOSDate = () => {
-    setDate(date.toDateString());
-    toggleDatePicker();
+  const handleConfirm = (date) => {
+    setSelectedDate(date);
+    hideDatePicker();
   };
 
   useEffect(() => {
-    console.log("DATE >", date);
-  }, [date]);
+    console.log("DATE >", selectedDate);
+  }, [selectedDate]);
 
   return (
     <>
       <Text style={[profileEdit.labelInput]}>{label}</Text>
       {label.includes("Дата рождения") ? (
-        <View style={{ alignSelf: "flex-start" }}>
-          {showPicker && (
-            <DateTimePicker
-              mode="date"
-              display="spinner"
-              value={date}
-              onChange={onChange}
-              style={{ height: 120, marginTop: -10 }}
-            />
-          )}
-
-          {/* FOR IOS */}
-          {showPicker && Platform.OS === "ios" && (
-            <View
-              style={{ flexDirection: "row", justifyContent: "space-around" }}
-            >
-              <TouchableOpacity onPress={toggleDatePicker}>
-                <Text>Cancel</Text>
-              </TouchableOpacity>
-              <TouchableOpacity onPress={confirmIOSDate}>
-                <Text>Confirm</Text>
-              </TouchableOpacity>
-            </View>
-          )}
-
-          {!showPicker && (
-            <Pressable>
-              <TextInput
-                style={profileEdit.textInput}
-                placeholder="10.01.2000"
-                value={date}
-                onChangeText={setDate}
-                editable="false"
-                onPressIn={toggleDatePicker}
-              />
-            </Pressable>
-          )}
-        </View>
+        <>
+          <Text
+            style={profileEdit.textInput}
+            onPress={() => setDatePickerVisible(true)}
+          >
+            {selectedDate
+              ? selectedDate
+                  .toLocaleDateString()
+                  .replace("/", ".")
+                  .replace("/", ".")
+              : "No date selected"}
+          </Text>
+          <DateTimePickerModal
+            date={selectedDate}
+            isVisible={datePickerVisible}
+            mode="date"
+            onConfirm={handleConfirm}
+            onCancel={hideDatePicker}
+            display="inline"
+          />
+        </>
       ) : (
         <TextInput
           style={profileEdit.textInput}
